b7dddf084b46391f842dca8f2911ec99d44a73e2
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-01-28 18:34:32 +0530

    Add files via upload

---
+++ add2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class add2
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+int a,b,c;
+System.out.println("enter a value");
+a=sc.nextInt();
+System.out.println("enter b value");
+b=sc.nextInt();
+c=a+b;
+System.out.println("addition="+c);
+}
+}
