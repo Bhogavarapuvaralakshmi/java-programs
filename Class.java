f2955712a7b85b6ba6f1bca8dcfad331ddbc5b6e
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-01-29 19:17:25 +0530

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,17 @@
+class Cla
+{
+int a=10,b=20;
+void print()
+{
+System.out.println("a="+a);
+System.out.println("b="+b);
+}
+}
+class Class
+{
+public static void main(String[]args)
+{
+Cla C=new Cla();
+C.print();
+}
+}
