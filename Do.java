fcc5be2e049e9d4de3b6388211876113c90fdfc6
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-01-30 18:16:34 +0530

    Add files via upload

---
+++ Do.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Do
+{
+public static void main(String[]args)
+{
+int i=20;
+System.out.println("the number are");
+do 
+{
+System.out.println(i+" ");
+i++;
+}while(i<=30);
+}
+}
