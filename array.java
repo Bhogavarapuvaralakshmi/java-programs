ac7ac032b88ee7cfff0c23b7aadd987a6f361c83
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-02-01 18:17:49 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class array
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+int a[]=new int[5];
+for(int i=0;i<5;i++)
+{
+a[i]=sc.nextInt();
+}
+System.out.println("The elements are");
+for(int i=0;i<5;i++)
+{
+System.out.print(a[i]+" ");
+}
+}
+}
