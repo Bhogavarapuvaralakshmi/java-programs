548bea965d7c24c2700f1c977e3f84c5be0f881b
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-02-02 18:19:20 +0530

    Add files via upload

---
+++ array1d.java
@@ -0,0 +1,12 @@
+class array1d
+{
+public static void main(String[]args)
+{
+int a[]={1,2,3,4,5};
+System.out.println("the elements are");
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
