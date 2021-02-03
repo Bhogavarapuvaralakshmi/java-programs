d4b9a6c7da8f5c886337b600f50e19a189bff627
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-02-03 19:05:13 +0530

    Add files via upload

---
+++ array2d.java
@@ -0,0 +1,15 @@
+class array2d
+{
+public static void main(String[]args)
+{
+int a[][]={{1,2},{3,4}};
+System.out.println("the elements are");
+for(int i=0;i<2;i++)
+{
+for(int j=0;j<2;j++)
+{
+System.out.println(a[i][j]);
+}
+}
+}
+}
