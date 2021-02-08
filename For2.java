07a3213425d9ec2387647514dbfae4434d202eaf
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-02-08 17:43:44 +0530

    Add files via upload

---
+++ For2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class For
+{
+public static void main(String[]args)
+{
+int i;
+System.out.println("the numbers are");
+for(i=1;i<=5;i++)
+{
+System.out.println("varalakshmi");
+}
+}
+}
