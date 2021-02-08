9f6dd16d27c806f65568f0eb09a19df8e5374b5a
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-02-08 17:43:09 +0530

    Add files via upload

---
+++ For1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.*;
+class For1
+{
+public static void main(String[]args)
+{
+int i,n;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter n value");
+n=sc.nextInt();
+for(i=1;i<=n;i++)
+{
+System.out.println(i+" ");
+}
+}
+}
