f40cfe04f7668e0be933cf7b765bc37e92215de6
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-02-13 17:40:29 +0530

    Add files via upload

---
+++ While2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.*;
+class While2
+{
+public static void main(String[]args)
+{
+int i=1,n;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter n value");
+n=sc.nextInt();
+while(i<=n)
+{
+System.out.println("varalakshmi");
+i++;
+}
+}
+}
