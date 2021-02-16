b8b2ac423f246bf5dd5c711d5ad00304acc63f73
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-02-16 20:48:58 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class string
+{
+public static void main(String[]args)
+{
+String s1,s2="lakshmi";
+Scanner x=new Scanner(System.in);
+System.out.println("enter your name");
+s1=x.nextLine();
+System.out.println(s1+" "+s2);
+int a=x.nextInt();
+System.out.println(Math.sqrt(a));
+}
+}
