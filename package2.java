1e92d0b9dbba5c37e9b60847c53cf846f975fa14
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-02-19 18:44:11 +0530

    Add files via upload

---
+++ package2.java
@@ -0,0 +1,14 @@
+import p3.student1;
+import p3.marks1;
+class package2
+{
+public static void main(String[]args)
+{
+student1 x=new student1();
+marks1 y=new marks1();
+x.rno(10);
+y.getmarks(30,30,30);
+x.display();
+y.display();
+}
+}
