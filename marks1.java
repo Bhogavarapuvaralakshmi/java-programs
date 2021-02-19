dbc28f5431b0f66e85529412163d5123f7fb0432
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-02-19 18:42:54 +0530

    Add files via upload

---
+++ marks1.java
@@ -0,0 +1,17 @@
+package p3;
+public class marks1
+{
+public int m1,m2,m3;
+public void getmarks(int a,int b,int c)
+{
+m1=a;
+m2=b;
+m3=c;
+}
+public void display()
+{
+System.out.println("m1="+m1);
+System.out.println("m2="+m2);
+System.out.println("m3="+m3);
+}
+}
