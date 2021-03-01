576d8d2ffd2e78a0cb0388ae0e2ed601e24511dd
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-03-01 14:38:14 +0530

    Add files via upload

---
+++ poly.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.applet.*;
+public class poly extends Applet 
+{
+int x1[]={20,120,220,20};
+int x2[]={120,220,220,120};
+int y1[]={20,120,20,20};
+int y2[]={120,20,220,120};
+int n1=4;
+int n2=4;
+public void paint(Graphics g)
+{
+g.drawPolygon(x1,y1,n1);
+g.fillPolygon(x2,y2,n2);
+}
+}
