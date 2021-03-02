190e415d9e8b36e055f73790b34c618bb8412d9d
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-03-02 15:50:17 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class Rectangle
+{
+int l,b;
+void get(int p,int q)
+{
+l=p;
+b=q;
+}
+int area()
+{
+return(l*b);
+}
+}
+class Rectangel1
+{
+public static void main(String[]args)
+{
+Rectangle r=new Rectangle();
+r.get(10,20);
+System.out.println("area of rectangle="+r.area());
+}
+}
