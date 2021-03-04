852dc6562f826b33e51801cc52181869cef84673
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-03-04 10:30:39 +0530

    Add files via upload

---
+++ sampleapplet.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.applet.*;
+/*<applet code="sampleapplet.class" width=300 height=300>
+</applet>*/
+public class sampleapplet extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawString("welcome to Applet",10,10);
+g.drawString("hii",20,20);
+}
+}
