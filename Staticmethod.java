9fc6291de9af6d578b5d4fcbef9a0829779cd863
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2021-03-04 10:31:25 +0530

    Add files via upload

---
+++ Staticmethod.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Statcis
+{
+static int x=10;
+static int cube()
+{
+return(x*x*x);
+}
+}
+class staticmethod 
+{
+public static void main(String args[])
+{
+System.out.println("cube="+calcute.cube());
+}
+}
